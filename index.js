71ec197235e1a52144d30889a9446fd894dd43bf
Rafael <rb2335@gmail.com>
2023-08-30 09:50:50 -0300

    add eslint

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const cors = require('cors')
 const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
+	console.log('Method:', request.method)
+	console.log('Path:  ', request.path)
+	console.log('Body:  ', request.body)
+	console.log('---')
+	next()
 }
 
 app.use(cors())
@@ -18,87 +18,87 @@ app.use(express.json())
 app.use(requestLogger)
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+	response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+	Note.find({}).then(notes => {
+		response.json(notes)
+	})
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
+	Note.findById(request.params.id)
+		.then(note => {
 
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
+			if (note) {
+				response.json(note)
+			} else {
+				response.status(404).end()
+			}
+		})
 
-    .catch(error => next(error))
+		.catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+	Note.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response, next) => {
-  const body = request.body
+	const body = request.body
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
+	if (body.content === undefined) {
+		return response.status(400).json({ error: 'content missing' })
+	}
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
+	const note = new Note({
+		content: body.content,
+		important: body.important || false,
+	})
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+	note.save()
+		.then(savedNote => {
+			response.json(savedNote)
+		})
+		.catch(error => next(error))
 })
 
 // toggle note importance
 app.put('/api/notes/:id', (request, response, next) => {
-  const { content, important } = request.body
+	const { content, important } = request.body
 
-  Note.findByIdAndUpdate(
-    request.params.id, 
-    { content, important },
-    { new: true, runValidators: true, context: 'query' }
-  ) 
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+	Note.findByIdAndUpdate(
+		request.params.id,
+		{ content, important },
+		{ new: true, runValidators: true, context: 'query' }
+	)
+		.then(updatedNote => {
+			response.json(updatedNote)
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+	console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
 
-  next(error)
+	next(error)
 }
 
 // this has to be the last loaded middleware.
@@ -106,5 +106,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
